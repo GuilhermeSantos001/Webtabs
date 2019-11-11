9b69eac7368ebfe5b10106fbb4fd70692a2631b4
Administrator <Administrator@MAVE.LOCAL>
2019-11-11 11:15:16 -0300

    Preparando a basa para a versão 3.0.0

--- remove_obsolete_branches.cmd
+++
@@ -1,5 +0,0 @@
-git checkout master
-git fetch
-git remote prune origin
-git branch --merged master | grep -v "master" | xargs git branch -d
-git branch -r --merged master | sed 's/ *origin\///' | grep -v "master" | xargs -I% git push origin :%
