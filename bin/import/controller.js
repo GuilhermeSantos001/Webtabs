b14c391b290e68022d1594ad68fcbb0abf145393
GuilhermeSantos001 <luizgp120@hotmail.com>
2020-08-23 15:45:26 -0300

    Inicio do algoritmo de historico de mudanças

--- bin/import/controller.js
+++ bin/import/controller.js
@@ -3,7 +3,23 @@ function CONTROLLER() {
 }
 
 CONTROLLER.versionSystem = 'v5.31.33-build';
+CONTROLLER.changelog = {};
 
+/**
+ * CHANGELOG v1.0
+ */
+CONTROLLER.changelog.paths = {};
+CONTROLLER.changelog.paths.folder = 'storage/changelog/';
+CONTROLLER.changelog.paths.file = filename => String(CONTROLLER.changelog.paths.folder + filename);
+CONTROLLER.changelog.data = {};
+CONTROLLER.changelog.data['1.0.0-beta'] = {
+    read: false,
+    log: '1123'
+};
+
+/**
+ * CONTROL FRAME v1.0
+ */
 CONTROLLER.frameEmpty = function () {
     let path = require('../import/localPath'),
         fs = require('fs');
@@ -28,6 +44,9 @@ CONTROLLER.frameEmpty = function () {
     }
 };
 
+/**
+ * ACTIONS v1.0
+ */
 CONTROLLER.defineAction = function (action, value) {
     CONTROLLER[action] = value
 };
